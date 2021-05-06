031a3b3ccc626bedd4d81ce5c811f0cfd8c06bfc
Sheba Ayinzabyonna <gensheba@gmail.com>
2021-05-06 10:30:05 +0300

    temperature

--- js/assignment4.js
+++ js/assignment4.js
@@ -1,25 +1,37 @@
-function temperature(a) {
-  let result;
-if (a < 33) {
-result = 'Normal temperature';
-} else {
-result = 'Go for a covid test';
+//function temperature(a) {
+//  let result;
+//if (a < 33) {
+//result = 'Normal temperature';
+//} else if (a = 35) {
+
+ // result='Watch your temperature';
+//}else(){
   
- }
- return result;
-}
-
-console.log(temperature(35));
-
-//if (a = 35) {
- //result ="Watch your temperature"
-
-//} else if (a = 33) {
-  //result ="Positive"
-//} else if (a > 37){
- // result="Go for Covid test"
-  
-//}else{
-//return result;
 //}
-//console.log(temperature(37))
+//result = 'Go for a covid test';
+  
+ //}
+ //return result;
+//}
+
+//console.log(temperature(35));
+
+function temperature(person){
+     switch(person.temperature){
+      case 33:
+      console.log("Normal");
+        break;
+      case 35:
+      console.log("Watch your temperature");
+         break;
+
+         default:
+           console.log("Kindly go for a COVID 19 test");
+          break;
+
+     }     
+     }
+
+     //temperature({temperature:33});
+     //temperature({temperature:35});
+     temperature({temperature:37});
