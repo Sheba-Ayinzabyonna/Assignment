eb861e11c0f8f544f52a4f53d13da86cde2dbeaa
Sheba Ayinzabyonna <gensheba@gmail.com>
2021-05-06 17:34:34 +0300

    reversed array

--- js/assignment4.js
+++ js/assignment4.js
@@ -2,13 +2,13 @@ function temperature(person){
      switch(person.temperature){
       case 33:
       console.log("Normal");
-        break;
+       break;
       case 35:
       console.log("Watch your temperature");
          break;
 
          default:
-           console.log("Kindly go for a COVID 19 test");
+          console.log("Kindly go for the COVID 19 test");
           break;
 
      }     
@@ -17,10 +17,8 @@ function temperature(person){
      temperature({temperature:33});
      temperature({temperature:35});
      temperature({temperature:37});
+    const number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25].reverse();
 
-
-const number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25].reverse();
-//let third =number[2];
 
 console.log(number);
 number.splice(8, 1,'Armstrong');
